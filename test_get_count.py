eca95ea6e3d7fa63661b38626119ae72a4db5477
Ahmed <114778876+ShinRwiz@users.noreply.github.com>
2024-05-26 00:58:37 +0100

    test

--- test_get_count.py
+++ test_get_count.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Test .get() and .count() methods.
+""" Test .get() and .count() methods
 """
 from models import storage
 from models.state import State
@@ -7,19 +7,5 @@ from models.state import State
 print("All objects: {}".format(storage.count()))
 print("State objects: {}".format(storage.count(State)))
 
-# Get all state instances
-all_states = storage.all(State)
-print("All State instances:", all_states)
-
-# Check if there are any state instances
-if all_states:
-    # Get the first state instance
-    first_state = list(all_states.values())[0]
-    first_state_id = first_state.id
-    print("First state ID:", first_state_id)
-
-    # Get the first state by ID
-    first_state_by_id = storage.get(State, first_state_id)
-    print("First state by ID:", first_state_by_id)
-else:
-    print("No State instances found.")
+first_state_id = list(storage.all(State).values())[0].id
+print("First state: {}".format(storage.get(State, first_state_id)))
